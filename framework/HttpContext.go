3fbf1435b8c1e7aa079644d3c59103f5e5351889
wufly <wfxykzd@163.com>
2020-07-04 00:57:35 +0800

    设置/获取context的值

--- framework/HttpContext.go
+++ framework/HttpContext.go
@@ -7,32 +7,49 @@ import (
 )
 
 type HttpContext struct {
-	Req *http.Request
-	Resp *responseWriter
-	store map[string]interface{}
+	Req        *http.Request
+	Resp       *responseWriter
+	store      map[string]interface{}
 	storeMutex *sync.RWMutex
 }
 
-func (ctx *HttpContext) JSON(data interface{})  {
-	ctx.Req.Header.Set("Context-type","application/json")
-	jsons,_ := json.Marshal(data)
-	_,_ = ctx.Resp.Write(jsons)
+func (ctx *HttpContext) JSON(data interface{}) {
+	ctx.Req.Header.Set("Context-type", "application/json")
+	jsons, _ := json.Marshal(data)
+	_, _ = ctx.Resp.Write(jsons)
 }
 
 // NewContext 创建context
-func NewContext(w http.ResponseWriter,r *http.Request)*HttpContext  {
+func NewContext(w http.ResponseWriter, r *http.Request) *HttpContext {
 	ctx := &HttpContext{}
-	ctx.Init(w,r)
+	ctx.Init(w, r)
 	return ctx
 }
 
 // Init 初始化context
-func (ctx *HttpContext) Init(w http.ResponseWriter,r *http.Request)  {
+func (ctx *HttpContext) Init(w http.ResponseWriter, r *http.Request) {
 	ctx.storeMutex = new(sync.RWMutex)
-	ctx.Resp = &responseWriter{w,0}
+	ctx.Resp = &responseWriter{w, 0}
 	ctx.Req = r
 	ctx.storeMutex.Lock()
 	ctx.store = nil
 	ctx.storeMutex.Unlock()
 }
 
+// Set 设置context的值
+func (ctx HttpContext) Set(key string, val interface{}) {
+	ctx.storeMutex.Lock()
+	if ctx.store == nil {
+		ctx.store = make(map[string]interface{})
+	}
+	ctx.store[key] = val
+	ctx.storeMutex.Unlock()
+}
+
+// Get 获取context的值
+func (ctx HttpContext) Get(key string) interface{} {
+	ctx.storeMutex.RLock()
+	val := ctx.store[key]
+	ctx.storeMutex.RUnlock()
+	return val
+}
